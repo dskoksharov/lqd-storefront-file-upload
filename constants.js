8d6c5bdf149bd950624fa040ee66e06656767d9d
MajesticThrust <majesticthrust@gmail.com>
2020-10-17 22:40:11 +0300

    Almost complete upload page

--- constants.js
+++ constants.js
@@ -1,4 +1,10 @@
 window.LQDGlobalConf = {
   supportEmail: "stores@vapeapp.net",
-  backendUrl: "",
-}
+
+  /**
+   * Backend url (no trailing slash - paths are manually concatenated)
+   */
+  // TODO change to prod backend
+  backendUrl: "http://3.129.104.96:8080", // test backend
+  // backendUrl: "http://3.129.157.184:8080" // prod backend
+};
