afb40a579e58a5b56cadcf77d5d7fc5a75328bb5
MajesticThrust <majesticthrust@gmail.com>
2020-10-18 22:17:21 +0300

    Polling, result table generation

--- dist/constants.js
+++ dist/constants.js
@@ -7,4 +7,14 @@ window.LQDGlobalConf = {
   // TODO change to prod backend
   backendUrl: "http://3.129.104.96:8080", // test backend
   // backendUrl: "http://3.129.157.184:8080" // prod backend
+
+  /**
+   * The interval (ms) with which the upload status is polled.
+   */
+  uploadStatusPollInterval: 10000,
+
+  /**
+   * How long an unconfirmed upload is considered valid.
+   */
+  uploadStatusInvalidationTimeout: 5 * 60 * 1000,
 };
