4ef0eebe16b4669568ab04a7fb76f173f3bc824a
Timur Mukhametulin <mt@finfort.ru>
2020-11-24 20:02:05 +0300

    New test backend address

--- dist/constants.js
+++ dist/constants.js
@@ -5,7 +5,7 @@ window.LQDGlobalConf = {
    * Backend url (no trailing slash - paths are manually concatenated)
    */
   // TODO change to prod backend
-  backendUrl: "https://dev.app.vapeapp.net:8443", // test backend
+  backendUrl: "https://test.app.vapeapp.net:8443", // test backend
   // backendUrl: "https://3.129.157.184:8080" // prod backend
 
   /**
